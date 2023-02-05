838520e7d91c4eb3b95e0a69703214966718d079
SergeyCodeJs <sergeysteh@gmail.com>
2023-02-05 16:35:48 +0100

    feat: add artist entity

---
+++ src/artist/artist.entity.ts
@@ -0,0 +1,30 @@
+import { v4 as uuid } from 'uuid';
+
+export class Artist {
+  private readonly id: string;
+  private name: string;
+  private grammy: boolean;
+
+  constructor(name: string, grammy: boolean) {
+    this.id = uuid();
+    this.name = name;
+    this.grammy = grammy;
+  }
+
+  public updateArtist(name: string, grammy: boolean): void {
+    this.name = name;
+    this.grammy = grammy;
+  }
+
+  public getArtistId() {
+    return this.id;
+  }
+
+  public getArtistName() {
+    return this.name;
+  }
+
+  public isArtistHasGrammy() {
+    return this.grammy;
+  }
+}
